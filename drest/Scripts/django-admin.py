d8847f79c828bcf013bde10465d735fd62c6c91c
chollingsworth <chollingsworth@hotmail.co.uk>
2021-05-16 17:32:10 -0500

    Commit to new project

---
+++ drest/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\dropbox\development\_projects\todo-django-rest-framework\drest\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
